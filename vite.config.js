9da073f5505e054609bd8626ff36b639c5160d7f
Dynamic-Capital <deathcoder8@gmail.com>
2025-09-09 16:50:36 +0500

    Add coming soon page with framer motion

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        comingsoon: resolve(__dirname, 'comingsoon.html'),
+      },
+    },
   },
 });
