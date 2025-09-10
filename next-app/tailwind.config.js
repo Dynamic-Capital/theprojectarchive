856f3912213f2fdbab6d868d14baaee5bb5e4d0f
Dynamic-Capital <deathcoder8@gmail.com>
2025-09-10 16:38:29 +0500

    chore: update theme colors and font

--- next-app/tailwind.config.js
+++ next-app/tailwind.config.js
@@ -1,7 +1,17 @@
 export default {
   content: ['./app/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: '#ff3d00',
+        text: '#070707',
+        background: '#373737',
+        muted: '#909195',
+      },
+      fontFamily: {
+        sans: ['"SFT Schrifted Sans"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
